7c499f29622a36bfa3f78e8ea006e409e3e4e330
wallytreats <nicklawrenceemail@gmail.com>
2017-09-06 11:04:32 -0600

    deploy test frontend

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -1,31 +1,31 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {
-          id: 1,
-          username: "wallytreats",
-          twitter: "wallytreats",
-          discord: "jfQ3kTd"
-        },
-        {
-          id: 2,
-          username: "xbeezlebubs",
-          twitter: "-placeholder",
-          discord: "-placeholder"
-        },
-        {
-          id: 3,
-          username: "badbadrobot",
-          twitter: "badbadrobot",
-          discord: "-placeholder"
-        }
-      ]);
-    })
-    .then(function (){
-      return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));")
-    })
-};
+// 
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('users').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('users').insert([
+//         {
+//           id: 1,
+//           username: "wallytreats",
+//           twitter: "wallytreats",
+//           discord: "jfQ3kTd"
+//         },
+//         {
+//           id: 2,
+//           username: "xbeezlebubs",
+//           twitter: "-placeholder",
+//           discord: "-placeholder"
+//         },
+//         {
+//           id: 3,
+//           username: "badbadrobot",
+//           twitter: "badbadrobot",
+//           discord: "-placeholder"
+//         }
+//       ]);
+//     })
+//     .then(function (){
+//       return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));")
+//     })
+// };
