a19c5ced140dbe5252d8574af7105b4df920d4d4
wallytreats <nicklawrenceemail@gmail.com>
2017-09-06 11:32:34 -0600

    add migrations

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
-// 'use strict'
-//
-// module.exports = {
-//   development: {
-//     client: 'pg',
-//     connection: 'postgres://localhost/wallybot'
-//   },
-//
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL
-//   }
-// };
+'use strict'
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/wallybot'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
