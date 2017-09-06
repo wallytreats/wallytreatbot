47da71d4e49d2c66b244e524372af41ae0b2f6ed
wallytreats <nicklawrenceemail@gmail.com>
2017-09-06 11:03:42 -0600

    deploy test frontend

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-  // 
-  // $('#postbtn').click(function(event){
-  //   event.preventDefault();
-  //   // console.log("hello");
-  //       var payload = {
-  //         username: document.getElementById("form").elements[0].value,
-  //         twitter: document.getElementById("form").elements[1].value,
-  //         discord: document.getElementById("form").elements[2].value
-  //       };
-  //       let options = {
-  //         contentType: 'application/json',
-  //         data: JSON.stringify(payload),
-  //         method: "POST",
-  //         url: "http://localhost:3800/"
-  //       }
-  //
-  //       $.ajax(options).done((data) => {
-  //         if(data.length === 0){
-  //           console.log("got nothin");
-  //         }
-  //       })
-  //       console.log(payload.username);
-  //
-  //   })
+  
+  $('#postbtn').click(function(event){
+    event.preventDefault();
+    // console.log("hello");
+        var payload = {
+          username: document.getElementById("form").elements[0].value,
+          twitter: document.getElementById("form").elements[1].value,
+          discord: document.getElementById("form").elements[2].value
+        };
+        let options = {
+          contentType: 'application/json',
+          data: JSON.stringify(payload),
+          method: "POST",
+          url: "http://localhost:3800/"
+        }
+
+        $.ajax(options).done((data) => {
+          if(data.length === 0){
+            console.log("got nothin");
+          }
+        })
+        console.log(payload.username);
+
+    })
